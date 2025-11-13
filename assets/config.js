e1ec3d3978920f3b0512776cd28d0d1170176fea
cm5wxjmcpv-bit <cm5wxjmcpv@privaterelay.appleid.com>
2025-11-13 13:42:56 -0500

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 // Site config (v2 - name+password login)
 const CONFIG = {
   siteKey: "simple-training-site-v2",
-  GAS_ENDPOINT: "https://script.google.com/macros/s/REPLACE_WITH_YOUR_DEPLOYMENT/exec",
+  GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbwRSkyCEsT8SUZLa2wff4gvxegzMYh6XUXKKqJbiDmPILP-BVJuyou4T2rOSbWK-GS_/exec",
   allowAnyUser: false,
   USERS: [
     { first: "Micah", last: "Lackey", password: "Password123" }
